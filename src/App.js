8a9ac1332fe83f70a61714516e2b044c6cdabc11
ShovalAtar <45339701+ShovalAtar@users.noreply.github.com>
2021-03-03 15:11:21 +0200

    nice looking, with empty reveal logic

--- src/App.js
+++ src/App.js
@@ -1,13 +1,14 @@
-import logo from "./logo.svg";
 import "./App.css";
 import React from "react";
 import Game from "../src/components/game.js";
 
 function App() {
   return (
-    <div className="App">
-      <img src={logo} className="App-logo" alt="logo" />
-      <Game></Game>
+    <div>
+      <div className="App">
+        <div className="Header">MY MINES</div>
+        <Game></Game>
+      </div>
     </div>
   );
 }
